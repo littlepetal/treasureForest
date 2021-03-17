c7b343d0727f9fa0e71289e60e2e9ae4fe1900f4
fergus Charles <ferguscharles7@gmail.com>
2021-03-17 15:36:43 +1100

    string outputted at once per second

--- Project_Docs/Exercise 3/exercise 3/Sources/main.asm
+++ Project_Docs/Exercise 3/exercise 3/Sources/main.asm
@@ -27,6 +27,8 @@ ROMStart    EQU  $4000  ; absolute address to place my code/constant data
 Counter     DS.W 1
 FiboRes     DS.W 1
 str         fcc "string"
+cr          fcb  $0D
+
 
 
 
@@ -55,11 +57,11 @@ mainLoop:
             ;movb #%00011100,SCI1CR1
             ;movb #%00000000,SCI1CR1 
             
-            ldaa #100
-            ;jsr   putcSCI0
             
-            ;jsr   delay
-            jsr   getcSCI0
+            jsr   putcSCI0
+            ldab #100
+            jsr   delay
+            ;jsr   getcSCI0
             ;jsr putchar
             ;jsr getchar
             bra mainLoop       
@@ -69,23 +71,24 @@ mainLoop:
 putcSCI0    
             movb #%00001000,SCI1CR2
             brclr SCI1SR1,#mSCI1SR1_TDRE,* 
-            movb #85,SCI1DRL
-            ldab SCI1DRL
-            stab $1500
+            movb x,SCI1DRL
+            ldab x
+            ldaa cr
+            sba
+            beq return
+            inx
+            bra putcSCI0
+return
             rts 
             
 
 
 delay          
-            
-            ldab #100
-inner       
-            decb
-            bne inner
-            
-            deca
-            bne delay
-            rts
+out_loop     ldx #60000 ;
+inner_loop 
+             dbne x,inner_loop
+             dbne b,out_loop
+             rts 
   
             
 
