c8cf4909af1e6a17dcb6902af3795048211c3c31
fergus Charles <ferguscharles7@gmail.com>
2021-03-29 11:22:40 +1100

    tidied up ex 1 with comments

--- Project_Docs/Exercise 1/code_fergus/exercise1/Sources/main.asm
+++ Project_Docs/Exercise 1/code_fergus/exercise1/Sources/main.asm
@@ -26,11 +26,12 @@ ROMStart    EQU  $4000  ; absolute address to place my code/constant data
 Counter     DS.W 1
 FiboRes     DS.W 1
 inpstr      fcc "a. bc. de!" 
-            fcb $0d
 terminating_character  fcb $0d
 
+space_store ds.B 1
+fullstop_store ds.B 1
 function_on ds 1
-;isletter:   ds $3000
+
 
 ; code section
             ORG   ROMStart
@@ -41,16 +42,17 @@ _Startup:
             LDS   #RAMEnd+1       ; initialize the stack pointer
 
             CLI                     ; enable interrupts
+
+
 mainLoop: 
-
-
 Charmanipulation:
-            ldx #inpstr
+            ldx #inpstr ;load the address on the string to register x
             ldaa #1
-            staa $4500
-            staa $4501 
-            jsr processstring
-            bra mainLoop 
+            staa space_store    ;set these flags to 1 to capitalise the first letter of each word for ex 1.3 and 1.4
+            staa fullstop_store ;
+            jsr processstring   ;jump to subroutine which handles the string
+            stop                ;end the program when the manipulation is done 
+                    ;
             
 
                    
@@ -62,76 +64,65 @@ Charmanipulation:
 ;check letter validity
 processstring:         ;loop through each letter and apply the required operation
                    
-            LDAB  0, x            
-            CMPB  terminating_character
-            BEQ   terminate
+            LDAB  0, x  ;load the first bit of x
+            CMPB  terminating_character  ;compare this with terminating character
+            BEQ   terminate  ;if end of stirng terminate
 keepProcessing:                   
             TBA
-            SUBA #65                           ;check if ascii code is less than 65, if yes, move on to next char
+            SUBA #65   ;check if ascii code is less than 65, if yes, move on to next char
             BLT  nextchar
                 
             TBA
             SUBA #90                           
-            BLT  validchar
+            BLT  validchar  ;uppercase
                   
             TBA
-            SUBA #97                           
+            SUBA #97         ;less than 97 invalid
             BLT  nextchar
                   
             TBA
-            SUBA #122                         
+            SUBA #122        ;more than 122 invalid
             BGE  nextchar
                                 
-            BRA validchar  
-validchar:
-            
-            ;need to come up with a way to do port h as input 
-            LDAA PTH 
-            staa $2000                             
-            LDAA  #1              ;0 for all caps, 1 for space to upper
-            LDAB  #0
-            sba
-            ;bra Capspace
-            ;rts Capspace
-            ;bra allToUpper
-            ;bra allToUpper        
-            ;bra   allToLower
-            bra fullspace
-                      
-           
-;test if spce char                   ;
-nextchar:   
+            BRA validchar    ;else lower
+validchar: ;if the char is valid perform operation     
+            ;bra allToUpper      ;upper
+            ;bra   allToLower    ;lower
+            bra fullspace        ;captical after full space
+            ;bra capspace        ; captialize every word
+                              ;
+nextchar:   ;in not alpha char, or operaction already performed, move to the next char
 
-    fullstopcheck:
+    fullstopcheck: ;before moving check if fullstop and store
             LDAB x
             TBA
             SUBA #46
             BNE spacecheck
             ldab #1
-            stab $4501
+            stab fullstop_store
             inx
             bra processstring
                   
             
-    spacecheck:
+    spacecheck:  ;before moving check if space and store
             LDAB x
             TBA
             SUBA #32
             BNE skip  
             ldaa #1
-            staa $4500
+            staa space_store
             inx
             bra processstring
     
-    ;else skip                        
+    ;else skip and move to next char  
     skip:       
             ldaa #0
-            staa $4500
-            staa $4501  
+            staa space_store   ;set fullstop and space flags to zero
+            staa fullstop_store  
             inx
-            bra processstring                   
+            bra processstring  ;loop back to next char manipulations
 
-terminate:        
+terminate: ;terminate reached if required
             rts
                    
                
@@ -142,47 +133,43 @@ allToUpper:
             ldab x
             TBA
             SUBA #90
-            BLE  storechar           
-            SUBB #32
+            BLE  storechar  ;if less than 90 must be upper given valid char, therefor skip manipulation
+            SUBB #32        ;else must be lower, subtract 32
 storechar:
-            stab x                        
+            stab x  ;store value in memory location
             BRA nextchar
             
-
-
 
 allToLower:
             ldab x
             TBA
             SUBA #90
-            BGT  storechar2            
-            ADDB #32
+            BGT  storechar2   ;if greater than 0 must be lower, therefor skip manipulation
+            ADDB #32          ;add 32 to make lower
 storechar2:
-            stab x 
+            stab x  ;store in memory location
             BRA nextchar
             
-
-
 
 Capspace 
            
            ldaa x
            ldab #1
-           subb $4500
-           BEQ allToUpper
-           BGT allToLower
+           subb space_store ;check the value of space flag
+           BEQ allToUpper   ;if it set, make sure the letter becomes upper case
+           BGT allToLower   ;if not set, must be lower case
            
             
 fullspace
           ;capitalise the first letter
           ldaa x
           ldab #1
-          subb $4500
-          BGT allToLower
+          subb space_store     ;check the space flag
+          BGT allToLower       ;if not set, fullstop must not be set either hence go to lower
           ldab #1
-          subb $4501 
-          BEQ allToUpper
-          BGT allToLower
+          subb fullstop_store  ;check fullstop flag
+          BEQ allToUpper       ;if set go to upper function
+          BGT allToLower       ;else go to lower
  
  
 
